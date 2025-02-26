485d54ad5c058ce3d8069b6271056eee5de66f10
moutoufadev <moutoufa.dev@gmail.com>
2025-02-26 16:00:12 +0000

    Create a new function named getComputerChoice. Write the code so that getComputerChoice will randomly return one of the following string values: “rock”, “paper” or “scissors”.

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,17 @@
 console.log('Hello World!')
+
+function getComputerChoice() {
+    const randomNum = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+
+    switch (randomNum) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+// Example usage
+console.log(getComputerChoice());
